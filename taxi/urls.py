544d27b24bdf1789f909c3d7c1f990c4086c3462
Oleksii Drenov <leshadrenev@gmail.com>
2025-07-12 09:16:15 +0300

    remove duplicate path

--- taxi/urls.py
+++ taxi/urls.py
@@ -52,26 +52,10 @@ urlpatterns = [
         name="toggle-car-assign",
     ),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/",
-        DriverDetailView.as_view(),
-        name="driver-detail"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/",
-        DriverDetailView.as_view(),
-        name="driver-detail"),
+    path("drivers/<int:pk>/",DriverDetailView.as_view(),name="driver-detail"),
     path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
-    path(
-        "drivers/<int:pk>/update/",
-        DriverLicenseUpdateView.as_view(),
-        name="driver-update",
-    ),
-    path(
-        "drivers/<int:pk>/delete/",
-        DriverDeleteView.as_view(),
-        name="driver-delete",
-    ),
+    path("drivers/<int:pk>/update/",DriverLicenseUpdateView.as_view(),name="driver-update",),
+    path("drivers/<int:pk>/delete/",DriverDeleteView.as_view(),name="driver-delete",),
 ]
 
 app_name = "taxi"
